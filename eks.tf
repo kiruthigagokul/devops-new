9165178fb6a3d956aec2621c8882d0a896b520c1
kiruthiga <kiruthigasarasulingam@example.com>
2026-02-18 14:44:17 +0530

    Initial commit

---
+++ eks.tf
@@ -0,0 +1,43 @@
+module "eks" {
+    source = "terraform-aws-modules/eks/aws"
+    version = "19.15.1"
+
+    cluster_name                   = local.name
+    cluster_endpoint_public_access = true
+
+    cluster_addons = {
+        coredns = {
+            most_recent = true
+        }
+        kube_proxy = {
+            most_recent = true
+        }
+        vpc_cni = {
+            most_recent = true
+        }
+    }
+    vpc_id = module.vpc.vpc_id
+    subnet_ids = module.vpc.private_subnets
+    control_plane_subnet_ids = module.vpc.intra_subnets
+
+    eks_managed_node_group_defaults = {
+        ami_type = "AL2023_X86_64_STANDARD"
+        instance_types = ["t2.medium"]
+        attach_cluster_primary_security_group = true
+    }
+
+    eks_managed_node_groups = {
+    gk_worker_node = {
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+      instance_types = ["t2.medium"]
+      capacity_type = "SPOT"
+
+      tags = {
+        ExtraTag = "helloworld"
+      }
+    }
+  }
+  tags = local.tags
+}
---
+++ provider.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+    backend "s3" {
+        bucket = "<bucket-name>"
+        key    = "<folder/terraform.tfstate>"
+        region = "ap-south-1"
+    }
+}
+provider "aws" {
+    region = "ap-south-1"
+}
+
+locals {
+    region = "ap-south-1"
+    name = "<cluster_name>"
+    vpc_cidr = "10.123.0.0/16"
+    azs =["ap-south-1a", "ap-south-1b"]
+    public_subnets = ["10.123.1.0/24", "10.123.2.0/24"]
+    private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+    intra_subnets = ["10.123.5.0/24", "10.123.6.0/24"]
+    tags = {
+        Example = local.name
+    }
+}
